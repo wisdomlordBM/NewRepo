2a32957bc327faa0adf97e8bfe1d3fa21326fa20
wisdomlord <wisdomlordbm@gmail.com>
2024-12-04 17:36:41 +0100

    update

--- Jobportalwebsite/wwwroot/js/site.js
+++ Jobportalwebsite/wwwroot/js/site.js
@@ -1,20 +1,2 @@
-﻿//document.querySelectorAll('.mark-read-btn').forEach(button => {
-//    button.addEventListener('click', function () {
-//        var notificationId = this.getAttribute('data-id');
-
-//        fetch('/admin/markAsRead', {
-//            method: 'POST',
-//            body: JSON.stringify({ id: notificationId }),
-//            headers: {
-//                'Content-Type': 'application/json'
-//            }
-//        })
-//            .then(response => response.json())
-//            .then(data => {
-//                if (data.success) {
-//                    this.closest('.notification').classList.add('read'); // Mark notification as read
-//                }
-//            });
-//    });
-//});
+﻿
 
